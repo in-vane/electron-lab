4ecc3282f0d5952ada7f44c566935983ee17d6b9
guzijie <1661873784@qq.com>
2024-03-16 22:28:09 +0800

    feat: update return value(age:return excel_base64,now:return image_base64)

--- python/tasks/check_ce/mode_danmark/change_excel.py
+++ python/tasks/check_ce/mode_danmark/change_excel.py
@@ -1,16 +1,34 @@
-from openpyxl import load_workbook
+import os
+import base64
+from io import BytesIO
+
+import fitz
+import openpyxl
 from openpyxl.styles import Border, Side
+import win32com.client as win32
+from PIL import ImageGrab
 
 from .get_table_message import all
 
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+EXCEL_PATH = os.path.join(CURRENT_PATH, "temp.xlsx")
+IMAGE_PATH = os.path.join(CURRENT_PATH, 'temp.png')
 
-def change_excel(excel, work_table, message_dict):
+
+def change_excel(wb, work_table, message_dict):
+    """
+    该函数主要是为了，呈现错误信息到吉盛标准ce表上
+    :param excel: 吉森标准ce表
+    :param work_table: ce表中的工作表
+    :param message_dict: 错误信息
+    :return:
+    """
     # 加载工作簿并选择工作表
-    wb = load_workbook(excel)
+    # wb = load_workbook(excel)
     sheet = wb[work_table]
 
     # 用绿色定义边框样式
-
+    # 用绿色定义边框样式
     green_border = Border(
         left=Side(style='thick', color='0000FF'),
         right=Side(style='thick', color='0000FF'),
@@ -42,10 +60,80 @@ def change_excel(excel, work_table, message_dict):
                             row_cell.border = green_border
 
     # 保存文件
-    wb.save('output.xlsx')
+    # wb.save(EXCEL_PATH)
 
 
-def checkTags(excel, work_table, pdf_path):
-    work_table = '例3'
-    message_dict = all(excel, work_table, pdf_path)
-    change_excel(excel, work_table, message_dict)
+# 将ecxel转化为图片
+def export_excel_range_to_image(excel_path, image_path, sheet_name):
+    """
+    导出Excel文件中指定工作表的特定范围为图片。
+
+    参数:
+    - excel_path: Excel文件的路径。
+    - image_path: 生成的图片保存路径。
+    - sheet_name: 工作表
+    """
+    range_string = "A1:H24"
+    # 确保Excel应用程序不可见以加快处理速度
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    excel.Visible = False
+    try:
+        # 打开工作簿
+        wb = excel.Workbooks.Open(os.path.abspath(excel_path))
+        sheet = wb.Sheets(sheet_name)
+        # 选择并复制指定范围
+        sheet.Range(range_string).CopyPicture(Appearance=1, Format=2)
+        # 从剪贴板获取图像
+        image = ImageGrab.grabclipboard()
+        # 检查剪贴板上是否有图像并保存
+        if image:
+            image.save(image_path, 'PNG')
+            print(f"图片已保存到 {image_path}")
+        else:
+            print("剪贴板上没有图像。")
+    except Exception as e:
+        print(f"发生错误：{e}")
+    finally:
+        # 关闭工作簿和Excel应用程序
+        wb.Close(SaveChanges=False)
+        excel.Quit()
+
+
+def checkTags(excel_file, pdf_file):
+    work_table = '例2'
+    # doc = fitz.open(pdf_file)
+    # wb = openpyxl.load_workbook(excel_file)
+    doc = fitz.open(stream=BytesIO(pdf_file))
+    wb = openpyxl.load_workbook(filename=BytesIO(excel_file))
+    wb.save(EXCEL_PATH)
+    message_dict = all(wb, work_table, doc)
+    change_excel(wb, work_table, message_dict)
+    # 将excel转化为图片，保存到IMAGE_PATG
+    export_excel_range_to_image(EXCEL_PATH, IMAGE_PATH, work_table)
+
+    # 创建一个内存中的二进制文件对象
+    # excel_buffer = BytesIO()
+    # 保存 Excel 文件到二进制对象中
+    # wb.save(excel_buffer)
+    # 将文件指针移至开头
+    # excel_buffer.seek(0)
+    # 将二进制数据编码为 Base64 字符串
+    # excel_base64 = base64.b64encode(excel_buffer.getvalue()).decode('utf-8')
+
+    # 将PNG图片加载为二进制流
+    with open(IMAGE_PATH, "rb") as image_file:
+        image_data = image_file.read()
+    # 将二进制流编码为Base64字符串
+    base64_encoded_data = base64.b64encode(image_data)
+
+    # 将Base64字节对象转换为字符串
+    image_base64 = base64_encoded_data.decode('utf-8')
+    print(image_base64)
+    os.remove(EXCEL_PATH)
+    os.remove(IMAGE_PATH)
+    # Close the workbook
+    # wb.close()
+
+    return image_base64
+
+# checkTags('2.xlsx','2.pdf')
